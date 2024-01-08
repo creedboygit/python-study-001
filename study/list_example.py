1b2e832aff4e66053b32efed096f999d39a843ea
creedboy <polozh@naver.com>
2024-01-08 21:03:41 +0900

    pip 업그레이드

---
+++ study/list_example.py
@@ -0,0 +1,35 @@
+animals = []
+
+print(animals)
+
+animals = ["사자", "호랑이", "고양이", "강아지"]
+
+print(animals)
+
+name = animals[3]
+
+print(name)
+
+animals.append("쥐")
+
+print(animals)
+
+del animals[1]
+
+print(animals)
+
+animals2 = animals[1 :3]
+
+print(animals2)
+
+animals3 = animals[0:3]
+
+print(animals3)
+
+animals_len = len(animals)
+
+print(animals_len)
+
+animals_len3 = len(animals3)
+
+print(animals_len3)
