4d4c168475c18f4e0cebd0adc97559836d182485
creedboy <polozh@naver.com>
2024-01-12 23:33:23 +0900

    add

--- study/selenium/selenium_login_test.py
+++ study/selenium/selenium_login_test.py
@@ -1,8 +1,13 @@
+import time
+
+import pyautogui
+import pyperclip
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 # from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 # 크롬 드라이버 자동 업데이트
@@ -21,5 +26,24 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # 웹페이지 해당 주소 이동
 driver.implicitly_wait(5)  # 웹페이지가 로딩될 때까지 5초 기다린다
+driver.maximize_window()  # 화면 최대화
 
 driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+
+# 아이디 입력창
+id = driver.find_element(By.CSS_SELECTOR, "#id")
+id.click()
+pyperclip.copy('1111')
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(2)
+
+# 비밀번호 입력창
+pw = driver.find_element(By.CSS_SELECTOR, "#pw")
+pw.click()
+pyperclip.copy('1111')
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(2)
+
+# 로그인 버튼
+login_btn = driver.find_element(By.CSS_SELECTOR, r"#log\.login")
+login_btn.click()
---
+++ study/selenium/selenium_login_test_02.py
@@ -0,0 +1,51 @@
+import time
+
+import pyautogui
+import pyperclip
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.chrome.options import Options
+
+# from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# 크롬 드라이버 자동 업데이트
+service = Service(excutable_path=ChromeDriverManager().install())
+
+# 브라우저 꺼짐 방지
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# 불필요한 에러 메시지 제거
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+# service = Service(excutable_path=ChromeDriverManager(driver_version="114.0.5735.90").install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+# driver = webdriver.ChromiumDriver(service=service)
+
+
+def clipboard_input(self, user_xpath, user_input):
+    pyperclip.copy(user_input)  # input을 클립보드로 복사
+    # driver.find_element_by_xpath(user_xpath).click()  # element focus 설정
+    driver.find_element(By.CSS_SELECTOR, user_xpath).click()  # element focus 설정
+    ActionChains(driver).key_down(Keys.CONTROL).send_keys("v").key_up(
+        Keys.CONTROL
+    ).perform()  # ctrl + v 전달
+
+
+driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+# IDxPath = '//*[@id="id"]'
+# PasswordxPath = '//*[@id="pw"]'
+IDxPath = "#id"
+PasswordxPath = "#pw"
+
+# input으로 따로 입력을 받을 예정이므로 미리 적지 않아도 됩니다
+ID = input("네이버 아이디: ")
+Password = input("네이버 비밀번호: ")
+
+clipboard_input(driver, IDxPath, ID)
+clipboard_input(driver, PasswordxPath, Password)
+# driver.find_element(By.CSS_SELECTOR, '//*[@value="로그인"]').click()
+driver.find_element(By.CSS_SELECTOR, r'#log\.login').click()
