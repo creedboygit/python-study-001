30756cfcc65a6e891e67f43353871f4cd4086c51
creedboy <polozh@naver.com>
2024-01-12 23:03:42 +0900

    add

---
+++ study/selenium/selenium_test.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# 크롬 드라이버 자동 업데이트
+service = Service(excutable_path=ChromeDriverManager().install())
+
+# 브라우저 꺼짐 방지
+chrome_options = Options()
+chrome_options.add_experimental_option('detach', True)
+
+# 불필요한 에러 메시지 제거
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+# service = Service(excutable_path=ChromeDriverManager(driver_version="114.0.5735.90").install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+# driver = webdriver.ChromiumDriver(service=service)
+driver.get("https://naver.com")
+
+
