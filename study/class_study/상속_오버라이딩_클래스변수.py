b078d53f124234288c13f8245a980408cdfafca5
creedboy <polozh@naver.com>
2024-01-17 21:16:38 +0900

    add

--- study/class_study/상속_오버라이딩_클래스변수.py
+++ study/class_study/상속_오버라이딩_클래스변수.py
@@ -1,8 +1,15 @@
+import random
+
+
 class Monster:
+
+    max_num = 1000
+
     def __init__(self, name, health, attack):
         self.name = name
         self.health = health
         self.attack = attack
+        Monster.max_num -= 1
 
     def move(self):
         print(f"[{self.name}] : 지상으로 이동")
@@ -19,15 +26,28 @@ class Shark(Monster):
 
 
 class Dragon(Monster):
+
+    # 생성자 오버라이딩
+    def __init__(self, name, health, attack):
+        super().__init__(name, health, attack)
+        self.skills = ("불뿜기", "꼬리치기", "날개치기")
+
     def move(self):
         print(f"[{self.name}] : 날기")
+
+    def skill(self):
+        print(f"[{self.name}] 스킬 사용 {self.skills[random.randint(0, 2)]}")
 
 
 wolf = Wolf("울프", 1500, 200)
 wolf.move()
+print(wolf.max_num)
 
 shark = Shark("샤크", 3000, 400)
 shark.move()
+print(shark.max_num)
 
 dragon = Dragon("드래곤", 8000, 300)
 dragon.move()
+dragon.skill()
+print(dragon.max_num)
