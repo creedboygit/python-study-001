c4c501d307e0a90cd4f860dd9041883d1966d8f0
creedboy <polozhzh@gmail.com>
2024-11-08 23:06:41 +0900

    commit

--- study/basic/dictionary_function.py
+++ study/basic/dictionary_function.py
@@ -5,11 +5,11 @@
 # 5. 어시스트
 
 play_data = {
-    'result': '승리',
-    'champ_name': '티모',
-    'kill': 13,
-    'death': 9,
-    'assist': 13,
+    "result": "승리",
+    "champ_name": "티모",
+    "kill": 13,
+    "death": 9,
+    "assist": 13,
 }
 
 for key in play_data.keys():
